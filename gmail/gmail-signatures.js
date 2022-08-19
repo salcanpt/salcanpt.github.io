aae8faae4d2d7b1ca2e15419817807275729e0ce
gormantec <craig@gormantec.com>
2022-08-19 16:02:44 +1000

    dddd

--- gmail/gmail-signatures.js
+++ gmail/gmail-signatures.js
@@ -230,9 +230,10 @@ function handleUpdateAllClick() {
         x = x.replaceAll("email@salcanpt.com", userList[i].primaryEmail);
         x = x.replaceAll("+61400000000", userList[i].recoveryPhone);
         let x2 = document.createElement("div");
-        x2.style.margin = "5px";
-        x2.style.padding = "5px";
+        x2.style.margin = "2px";
+        x2.style.padding = "3px";
         x2.style.backgroundColor = "white";
+        x2.style.width = "310px";
         x2.innerHTML = x;
         document.getElementById('rightPanel').appendChild(x2);
       }
