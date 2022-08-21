1dfd7a86856605c781b261bac37db9c56598d450
gormantec <craig@gormantec.com>
2022-08-21 13:52:55 +1000

    ffff

--- gmail/gmail-signatures.js
+++ gmail/gmail-signatures.js
@@ -396,12 +396,12 @@ async function handleSaveTemplateClick()
   let b1=document.createElement("button");
   let b2=document.createElement("button");
   let dItem=""+(count+4);
-  b2.onclick(()=>{
+  b2.onclick=()=>{
     localStorage.removeItem("template-"+dItem);
     document.getElementById("example"+dItem+"DeleteButton").remove();
     document.getElementById("example"+dItem+"Button").remove();
     document.getElementById("example"+dItem).remove();
-  });
+  };
   b1.id="example"+(count+4)+"Button";
   b1.onclick=handleExampleClick('example'+(i+4));
   b1.innerText="Use Example "+(i+4);
@@ -430,13 +430,14 @@ async function loadCachedTemplates()
       if(b64html)
       {
         let b1=document.createElement("button");
-        let b2=document.createElement("button");  let dItem=""+(count+4);
-        b2.onclick(()=>{
+        let b2=document.createElement("button");  
+        let dItem=""+(count+4);
+        b2.onclick=()=>{
           localStorage.removeItem("template-"+dItem);
           document.getElementById("example"+dItem+"DeleteButton").remove();
           document.getElementById("example"+dItem+"Button").remove();
           document.getElementById("example"+dItem).remove();
-        });
+        };
         b1.id="example"+(i+4)+"Button";
         b1.onclick=handleExampleClick('example'+(i+4));
         b1.innerText="Use Example "+(i+4);
