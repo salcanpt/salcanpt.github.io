0c85fbf7ad080eaa57dd0175fef45878c18494db
gormantec <craig@gormantec.com>
2022-08-23 15:55:44 +1000

    fffff

--- gmail/gmail-signatures.js
+++ gmail/gmail-signatures.js
@@ -198,7 +198,7 @@ async function handleUpdateAllClick() {
           x2.innerHTML = x;
           document.getElementById('rightPanelContent').appendChild(x2);
   
-          if (userListXX.primaryEmail && (userListXX.primaryEmail.startsWith("craig") || userListXX.primaryEmail.startsWith("luke"))) {
+          if (userListXX.primaryEmail) {
             console.log("Update:"+userListXX.primaryEmail);
             if (accessToken) {
               try {
